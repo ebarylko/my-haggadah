87811c08dc9be5d93cde3c330889ee9175db1137
EItan Barylko <eitan@barylko.com>
2023-05-10 19:45:22 -0700

    Added karma configutation

--- test/haggadah/events_test.cljs
+++ test/haggadah/events_test.cljs
@@ -1,23 +1,31 @@
 (ns haggadah.events-test
-  (:require [haggadah.events :as sut]
-            [haggadah.subs :as subs]
-            #_[haggadah.src.haggadah.subs :as sub]
+  (:require [haggadah.subs :as subs]
             [cljs.test :as t :include-macros true]
             [re-frame.core :as rf]
             [day8.re-frame.test :as rf-test]
-            #_[haggadah.events :as events]))
+            [haggadah.events :as events]))
+
+(defn test-fixtures
+  []
+  ;; change this coeffect to make tests start with nothing
+  (rf/reg-cofx
+   :local-store-todos
+   (fn [cofx _]
+     "Read in todos from localstore, and process into a map we can merge into app-db."
+     (assoc cofx :name
+            "(Unkown)"))))
 
 (t/deftest admin-login
   (rf-test/run-test-sync               ;; <-- add this
    ;; with the above macro this becomes a dispatch-sync
    ;; and app-db is isolated between tests
-   (rf/dispatch [:initialise-db])
+   #_(test-fixtures)
+   (rf/dispatch [::events/initialise-db])
    ;; Define subscriptions to the app state
    (let [name (rf/subscribe [::subs/name])]
      ;;Assert the initial state
-     #_(t/is (=  "(Unknown)" @name))
-     (rf/dispatch [::login])
-     (t/is (= "han@skywalker.com" @name)))))
+     (t/is (=  "(Unknown)" @name)))))
 
-
- (admin-login)
+(t/deftest equality
+  (t/is (= 2 3)))
+#_(t/run-tests)
