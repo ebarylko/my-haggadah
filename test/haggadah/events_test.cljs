b4e7d1595afa9bf906ee25e670f8bcea610d63d6
EItan Barylko <eitan@barylko.com>
2023-05-10 20:01:30 -0700

    Corrected name of event to initialize db

--- test/haggadah/events_test.cljs
+++ test/haggadah/events_test.cljs
@@ -5,27 +5,15 @@
             [day8.re-frame.test :as rf-test]
             [haggadah.events :as events]))
 
-(defn test-fixtures
-  []
-  ;; change this coeffect to make tests start with nothing
-  (rf/reg-cofx
-   :local-store-todos
-   (fn [cofx _]
-     "Read in todos from localstore, and process into a map we can merge into app-db."
-     (assoc cofx :name
-            "(Unkown)"))))
 
 (t/deftest admin-login
   (rf-test/run-test-sync               ;; <-- add this
    ;; with the above macro this becomes a dispatch-sync
    ;; and app-db is isolated between tests
-   #_(test-fixtures)
-   (rf/dispatch [::events/initialise-db])
+   (rf/dispatch [::events/initialize-db])
    ;; Define subscriptions to the app state
    (let [name (rf/subscribe [::subs/name])]
      ;;Assert the initial state
      (t/is (=  "(Unknown)" @name)))))
 
-(t/deftest equality
-  (t/is (= 2 3)))
-#_(t/run-tests)
+
