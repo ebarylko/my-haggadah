720255d68945a1a5b5deaa614b20a83125b37245
EItan Barylko <eitan@barylko.com>
2023-05-18 19:30:34 -0700

    Added integration test to see if admin credentials were added to database

--- test/acceptance/admin_login_test.clj
+++ test/acceptance/admin_login_test.clj
@@ -1,6 +1,14 @@
 (ns acceptance.admin-login-test
   (:require  [clojure.test :as t]
-             [etaoin.api :as e]))
+             [etaoin.api :as e])
+  (:import com.google.firebase.FirebaseApp
+            com.google.firebase.FirebaseOptions
+            com.google.firebase.FirebaseOptions$Builder
+            com.google.firebase.auth.FirebaseAuth
+            com.google.firebase.auth.FirebaseAuthException
+            com.google.firebase.auth.UserRecord
+            com.google.firebase.auth.UserRecord$CreateRequest
+            com.google.firebase.auth.UserRecord$UpdateRequest))
 
 (def driver (e/firefox-headless))
 
@@ -12,9 +20,33 @@
 
 ;; (e/go driver "http://localhost:8280/")
 
+(defn- convert-user-record-to-map
+  [^UserRecord user-record]
+  { :email (. user-record getEmail)
+   :email-verified (. user-record isEmailVerified)
+   :uid (. user-record getUid)
+   :provider-id (. user-record getProviderId)
+   :photo-url (. user-record getPhotoUrl)
+   :phone-number (. user-record getPhoneNumber)
+   :display-name (. user-record getDisplayName)
+   :disabled (. user-record isDisabled)})
+
+(defn create-user
+  [{:keys [email pwd]}]
+  (let [firebase-auth (. FirebaseAuth getInstance) 
+        create-request (doto (new UserRecord$CreateRequest)
+                         (.setEmail email)
+                         (.setPassword pwd))]
+    (convert-user-record-to-map (. firebase-auth createUser create-request))))
+
 
 (t/deftest message-test
  (t/testing "default user" 
    (let [_  (e/go driver "http://localhost:5000/")
          actual (e/get-element-text driver {:class :haggadah-styles-level1})]
-(t/is (= default-message actual)))))
+     (t/is (= default-message actual))))
+  (t/testing "admin user"
+    (create-user {:email "han@skywalker.com"  :pwd "123456789"})
+    (let [_  (e/click driver {:tag :button})
+          actual (e/get-element-text driver {:class :haggadah-styles-level1})]
+      (t/is (= admin-login-message actual)))))
