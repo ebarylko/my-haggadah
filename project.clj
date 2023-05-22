ba3f6313e06b2e24a3515d4f32d33eddb13ff6df
EItan Barylko <eitan@barylko.com>
2023-05-22 10:43:39 -0700

    Added cheshire dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [etaoin/etaoin "1.0.40"]
-                 [dakrone/cheshire "5.11.0"]
+                 [cheshire "5.11.0"]
+                 [clj-http "3.12.3"]
                  [environ "1.2.0"]
                  [alekcz/googlecredentials "3.0.1"]
                  [com.google.firebase/firebase-admin "9.1.1"]]
