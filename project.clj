9816e0daad75bdf79ef4300588b10ff810c0730e
EItan Barylko <eitan@barylko.com>
2023-05-21 18:11:15 -0700

    Added cheshire dependendy in project.clj, added build-options and init functions to admin_login_test

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [etaoin/etaoin "1.0.40"]
+                 [dakrone/cheshire "5.11.0"]
+                 [environ "1.2.0"]
+                 [alekcz/googlecredentials "3.0.1"]
                  [com.google.firebase/firebase-admin "9.1.1"]]
   :repl-options {:init-ns haggadah.core
                  :init (init)})
--- test/acceptance/admin_login_test.clj
+++ test/acceptance/admin_login_test.clj
@@ -1,15 +1,19 @@
 (ns acceptance.admin-login-test
   (:require  [clojure.test :as t]
-             [etaoin.api :as e])
+             [environ.core :refer [env]]
+             [etaoin.api :as e]
+             [googlecredentials.core :as gcred])
   (:import com.google.firebase.FirebaseApp
             com.google.firebase.FirebaseOptions
             com.google.firebase.FirebaseOptions$Builder
+            com.google.auth.oauth2.ServiceAccountCredentials
             com.google.firebase.auth.FirebaseAuth
             com.google.firebase.auth.FirebaseAuthException
             com.google.firebase.auth.UserRecord
             com.google.firebase.auth.UserRecord$CreateRequest
             com.google.firebase.auth.UserRecord$UpdateRequest))
 
+(println "Hello how are you"(System/getenv "GCLOUD_PROJECT"))
 
 (def driver (e/firefox-headless))
 
@@ -20,6 +24,42 @@
 "Hello from han@skywalker.com. This is the Home Page.We're glad to see you.")
 
 ;; (e/go driver "http://localhost:8280/")
+
+
+(println "Hello how are you"(System/getenv "GCLOUD_PROJECT"))
+
+
+(defn- build-firebase-options 
+  ([]
+   (try 
+     (-> (new FirebaseOptions$Builder) ;use thread-first when the final part of the function will return value to be used
+         (.setCredentials (gcred/load-service-credentials))
+         (.build))
+     (catch Exception e (println "\nError: FIREBASE_CONFIG/GOOGLE_APPLICATION_CREDENTIALS AND GOOGLE_CLOUD_PROJECT environment variables must both be set"))))
+  ([database-name]
+   (try 
+     (-> (new FirebaseOptions$Builder) ;use thread-first when the final part of the function will return value to be used
+         (.setCredentials ^ServiceAccountCredentials (gcred/load-service-credentials))
+         (.setDatabaseUrl (str "https://" database-name ".firebaseio.com"))
+         (.build))
+     (catch Exception e (println "\nError: FIREBASE_CONFIG/GOOGLE_APPLICATION_CREDENTIALS AND GOOGLE_CLOUD_PROJECT environment variables must both be set")))))
+
+(defn init []
+  (let [database-name (or (env :firebase-database) (env :google-cloud-project))]
+    (if (nil? database-name)
+      (try
+        (. FirebaseAuth getInstance) 
+        (catch IllegalStateException ise
+          (let [options (build-firebase-options)]
+            (. FirebaseApp initializeApp options)))))
+    (try
+      (. FirebaseAuth getInstance) 
+      (catch IllegalStateException ise
+        (let [options (build-firebase-options database-name)]
+          (. FirebaseApp initializeApp options))))))
+
+
+
 
 (defn- convert-user-record-to-map
   [^UserRecord user-record]
@@ -47,7 +87,8 @@
          actual (e/get-element-text driver {:class :haggadah-styles-level1})]
      (t/is (= default-message actual))))
   (t/testing "admin user"
-    (FirebaseApp/initializeApp (System/getenv "GCLOUD_PROJECT"))
+    (println "Hello how are you"(System/getenv "GCLOUD_PROJECT"))
+    (init)   
     (create-user {:email "han@skywalker.com"  :pwd "123456789"})
     (let [_  (e/click driver {:tag :button})
           actual (e/get-element-text driver {:class :haggadah-styles-level1})]
