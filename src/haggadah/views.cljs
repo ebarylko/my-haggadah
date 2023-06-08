59b4fd2f788ceda0d43b9099a363983001063801
EItan Barylko <eitan@barylko.com>
2023-06-07 18:27:51 -0700

    Added bulma

--- src/haggadah/views.cljs
+++ src/haggadah/views.cljs
@@ -13,16 +13,13 @@
 (goog-define WRITE false)
 
 
-(defn home-panel []
-  [:div.leading-normal.tracking-normal.text-white.gradient {:class (styles/home-page)}
-   [:nav {:id "header", :class "fixed w-full z-30 top-0 text-white"}
+(defn menu
+  []
+[:nav {:id "header", :class "fixed w-full z-30 top-0 text-white"}
     [:div {:class "w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2"}
-     [:div {:class "pl-4 flex items-center" }
-      "ourhaggadah.com "
-      [:a {:class "toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl", :href "#"}
-       [:svg {:class "h-8 fill-current inline", :xmlns "http://www.w3.org/2000/svg", :viewBox "0 0 512.005 512.005"}
-        [:rect {:fill "#2a2a31", :x "16.539", :y "425.626", :width "479.767", :height "50.502", :transform "matrix(1,0,0,1,0,0)"}]
-        [:path {:class "plane-take-off", :d " M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "}]]]]
+     [:a {:class "pl-4 flex items-center"
+          :on-click  #(re-frame/dispatch [::events/navigate :home])} "ourhaggadah.com "]
+     
      [:div {:class "block lg:hidden pr-4"}
       [:button {:id "nav-toggle", :class "flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"}
        [:svg {:class "fill-current h-6 w-6", :viewBox "0 0 20 20", :xmlns "http://www.w3.org/2000/svg"}
@@ -31,13 +28,16 @@
      [:div {:class "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20", :id "nav-content"}
       [:ul {:class "list-reset lg:flex justify-end flex-1 items-center"}
        [:li {:class "mr-3"}
-        [:a {:class "inline-block py-2 px-4 text-black font-bold no-underline", :href "#"} "Active"]]
+        [:a {:class "inline-block py-2 px-4 text-black font-bold no-underline", :on-click #(re-frame/dispatch [::events/navigate :about])} "About"]]
        [:li {:class "mr-3"}
         [:a {:class "inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4", :href "#"} "link"]]
        [:li {:class "mr-3"}
         [:a {:class "inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4", :href "#"} "link"]]]
       [:button {:id "navAction", :class "mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"} ]]]
-    [:hr {:class "border-b border-gray-100 opacity-25 my-0 py-0"}]] 
+    [:hr {:class "border-b border-gray-100 opacity-25 my-0 py-0"}]])
+(defn home-panel []
+  [:div.leading-normal.tracking-normal.text-white.gradient {:class (styles/home-page)}
+   [menu] 
 [:div {:class "pt-24"}
  [:div {:class "container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center"}  
   [:div {:class "flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"}
@@ -272,12 +272,6 @@ Please login below to access your haggadot."]
 (defmethod routes/panels :home-panel [] [home-panel])
 ;; about
 
-(defn about-panel []
-  [:div
-   [:div
-    [:button {:on-click #(re-frame/dispatch [::events/navigate :home]) :class (styles/button)}
-     "Return to the Home Page"]]
-   [:h1 {:class (styles/header)} "This is the About Page."]])
 
 (defn haggadah-panel []
   [:div 
@@ -293,28 +287,6 @@ Please login below to access your haggadot."]
       
 )]])
 
-(defn menu
-  []
-[:nav {:id "header", :class "fixed w-full z-30 top-0 text-white"}
-    [:div {:class "w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2"}
-     [:a {:class "pl-4 flex items-center"
-          :on-click  #(re-frame/dispatch [::events/navigate :home])} "ourhaggadah.com "]
-     
-     [:div {:class "block lg:hidden pr-4"}
-      [:button {:id "nav-toggle", :class "flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"}
-       [:svg {:class "fill-current h-6 w-6", :viewBox "0 0 20 20", :xmlns "http://www.w3.org/2000/svg"}
-        [:title "Menu"]
-        [:path {:d "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"}]]]]
-     [:div {:class "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20", :id "nav-content"}
-      [:ul {:class "list-reset lg:flex justify-end flex-1 items-center"}
-       [:li {:class "mr-3"}
-        [:a {:class "inline-block py-2 px-4 text-black font-bold no-underline", :href "#"} "Active"]]
-       [:li {:class "mr-3"}
-        [:a {:class "inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4", :href "#"} "link"]]
-       [:li {:class "mr-3"}
-        [:a {:class "inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4", :href "#"} "link"]]]
-      [:button {:id "navAction", :class "mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"} ]]]
-    [:hr {:class "border-b border-gray-100 opacity-25 my-0 py-0"}]])
 
 (str "Hello " "name" ". We're glad to see you")
 
@@ -332,25 +304,26 @@ Please login below to access your haggadot."]
       [:p {:class "leading-normal text-2xl mb-8"} "Click the button below so you can see your haggadot and share them"]
       [:button {:class "mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
                 :on-click  #(re-frame/dispatch [::events/login :admin]) :data-test-id "load-haggadah"} "Load haggadah"]]
-     [:div.shadow-lg.flex-auto.text-align-left
+     [:div.shadow-lg.flex-auto.text-align-left.bg-white
       (let [{:keys [haggadah-text]} @(re-frame/subscribe [::subs/haggadah-text])]
         (when haggadah-text
-          [:div.border-black.border-2.p-3  {:dangerouslySetInnerHTML #js{:__html (js/marked.parse haggadah-text)} :id "haggadah-text"}]))]]]]
+          [:div.border-black.border-2.p-3.text-black  {:dangerouslySetInnerHTML #js{:__html (js/marked.parse haggadah-text)} :id "haggadah-text"}]))]]]])
 
 
-  #_[:div
-     [:button { :class (styles/button)}
-      "Return to Home Page"]
-     (let [name (re-frame/subscribe [::subs/name])]
-       [:div
-        [:h1.user (str "Hello from " @name ". This is the Home Page." "We're glad to see you.")]
-        (let [{:keys [haggadah-text]} @(re-frame/subscribe [::subs/haggadah-text])]
-          (when haggadah-text
-            [:div  {:dangerouslySetInnerHTML #js{:__html (js/marked.parse haggadah-text)} :id "haggadah-text"}]))])
-     [:h1 {:class (styles/header)}"To see your dashboard please click on the button below"]
-     [:div 
-      [:button {:on-click #(re-frame/dispatch [::events/login :admin]) :id "load-user"
-                :class (styles/button)} "Load user"]]])
+(defn about-panel
+  []
+  [:div.leading-normal.tracking-normal.text-white.gradient.h-screen {:class (styles/home-page)}
+   [menu]
+   [:div {:class "lg:container lg:mx-auto md:container"}
+    [:div.pt-24.text-center
+     [:h1.text-5xl "Why ourhaggadah.com?"]
+     [:h1.pt-4.text-2xl "Creating a haggadah is daunting. There are so many questions to answer, such as what to include, who reads what portions, and how to accomodate speakers of different languages. I wanted to make the process easier so that people can focus on celebrating pesach with their friends and family."]]
+    [:div.pt-24.text-center.pt-10
+
+     [:h1.text-5xl "About me"]
+     [:div   {:class "w-full md:w-3/5 py-6 text-center"}
+      [:img {:class "w-full md:w-4/5 z-50", :src "/images/about-page-photo.jpeg"}]]
+     [:h1.pt-4.text-2xl "My name is Eitan Barylko and I am an undergraduate student at Simon Fraser University. I love programming, cooking, and complaining that all the interesting things are in Vancouver. If you want to see what else I've worked on, you can checkout my github repo on the link below."]]]])
 
 (defmethod routes/panels :haggadah-panel [] [haggadah-panel])
 (defmethod routes/panels :about-panel [] [about-panel])
