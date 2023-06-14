92bec4a201b5c90b2fe0d07c633308a68c0de34a
EItan Barylko <eitan@barylko.com>
2023-06-13 19:34:54 -0700

    Added ws dependency

--- src/haggadah/fb/firestore.cljs
+++ src/haggadah/fb/firestore.cljs
@@ -11,9 +11,10 @@
   @firestore)
 
 (defn init [firebase-instance]
-  (println  FIREBASE_FIRESTORE_HOST)
+  
   (reset! firestore (fs/getFirestore firebase-instance))
   (when FIREBASE_FIRESTORE_HOST
+    (println  "Connecting to firestore host " FIREBASE_FIRESTORE_HOST)
     (let [[host port] (clojure.string/split FIREBASE_FIRESTORE_HOST #":")]
       (fs/connectFirestoreEmulator @firestore host (js/parseInt port)))))
 
