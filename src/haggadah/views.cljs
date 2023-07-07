503679f8db63db4e45849d948992594478730dee
EItan Barylko <eitan@barylko.com>
2023-07-07 00:02:33 -0700

    Changed the dashboard so page content is higher up the page

--- src/haggadah/views.cljs
+++ src/haggadah/views.cljs
@@ -32,12 +32,14 @@
      [:h1.navbar-item.is-size-5.has-text-weight-bold "ourhaggadah"]
      [:a.navbar-item.is-size-5.has-text-weight-bold {:on-click  #(re-frame/dispatch [::push-state :home])} "Home"]
      [:a.navbar-item.is-size-5.has-text-weight-bold {:on-click  #(re-frame/dispatch [::push-state :about])} "About"]
+     [:a.navbar-item.is-size-5.has-text-weight-bold {:on-click #(re-frame/dispatch [::events/signout])} "Sign out"]
      [:a {:role "button", :class "navbar-burger", :aria-label "menu", :aria-expanded "false", :data-target "navbarBasicExample"}
       [:span {:aria-hidden "true"}]
       [:span {:aria-hidden "true"}]
       [:span {:aria-hidden "true"}]]]
     [:div {:id "navbarBasicExample", :class "navbar-menu"}
      [:div {:class "navbar-start"}
+      
       [:a {:class "navbar-item"} "Documentation"]
       [:div {:class "navbar-item has-dropdown is-hoverable"}
        [:a {:class "navbar-link"} "More"]
@@ -122,48 +124,45 @@
          [:a.button.is-small {:class (styles/submit-button) :on-click  #(re-frame/dispatch [::events/login]) :data-testid :submit} "Submit"]]]
        ]]]]])
 
-
+(defn wave-bottom
+  []
+[:div {:class "relative -mt-12 lg:-mt-24"}
+       [:svg {:viewBox "0 0 1428 174", :version "1.1", :xmlns "http://www.w3.org/2000/svg", :xmlns:xlink "http://www.w3.org/1999/xlink"}
+        [:g {:stroke "none", :stroke-width "1", :fill "none", :fill-rule "evenodd"}
+         [:g {:transform "translate(-2.000000, 44.000000)", :fill "#FFFFFF", :fill-rule "nonzero"}
+          [:path {:d "M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496", :opacity "0.100000001"}]
+          [:path {:d "M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z", :opacity "0.100000001"}]
+          [:path {:d "M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z", :id "Path-4", :opacity "0.200000003"}]]
+         [:g {:transform "translate(-4.000000, 76.000000)", :fill "#FFFFFF", :fill-rule "nonzero"}
+          [:path {:d "M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"}]]]]])
 
 (defn dashboard-panel
   []
   [:div {:class (styles/home-page)}
-   [:section.hero.is-medium.container
-    [:div.pt-24.hero-body
-     (let [name (re-frame/subscribe [::subs/name])]
-       [:div
-        [:h1.text-center.is-size-4 {:data-testid :user}
-         (str "Hello " @name ". Welcome to your dashboard. To make a new haggadah, click the button to your right. To share and edit your existing haggadah, look at your haggadot below ")]])
-     [:div.pl-6.buttons.is-right
-      [:a.button.is-medium.is-focused.is-pulled-right {:data-testid :create-haggadah
-                                                      :on-click #(re-frame/dispatch [::push-state :haggadah-creation])}   "Create haggadah"]]
-     [:div.pl-6.buttons.is-right
-      [:a.button.is-medium.is-focused.is-pulled-right {:data-testid "signout"
-                                                      :on-click #(re-frame/dispatch [::events/signout])} "Signout"]]
-     [:div
-      [:h1.is-size-3
-       "Here are the haggadot you have created"]
-      (let [haggadot @(re-frame/subscribe [::subs/haggadot])]
-        (when haggadot
-          [:ul
-           (for [{:keys [title id]} haggadot :when id] 
-             ^{:key id}[:li.mb-2
-                        [:a {:data-testid (str "view-" id)
-                             :href (href :haggadah-view {:id id})} title]
-                        [:a.is-small.ml-2 {:data-testid (str "edit-" id )
-                                           :href (href :haggadah-edit {:id id})}
-                         [:i.fas.fa-edit]]])]))] ]
-    [:div
-     [:h1.is-size-3
-      "Here are the haggadot you have created"]
-     (let [haggadot @(re-frame/subscribe [::subs/haggadot])]
-       (when haggadot
-         [:ul
-          (for [{:keys [title id]} haggadot :when id] 
-            ^{:key id}[:li.mb-2
-                       [:a {:data-testid (str "view-" id)
-                            :href (href :haggadah-view {:id id})} title]
-                       [:a.button.is-small.ml-2 {:data-testid (str "edit-" id )
-                                                 :href (href :haggadah-edit {:id id})} "Edit " title]])]))]]])
+   [:div.container.is-large.hero.is-flex
+    [:div.hero-body.pt-6
+     [:div.pt-24.column
+      (let [name (re-frame/subscribe [::subs/name])]
+        [:div
+         [:h1.text-center.is-size-4 {:data-testid :user}
+          (str "Hello " @name ". Welcome to your dashboard. To make a new haggadah, click the button to your right. To share and edit your existing haggadah, look at your haggadot below ")]])
+      [:div.pl-6.buttons.is-right
+       [:a.button.is-medium.is-focused.is-pulled-right.mt-2 {:data-testid :create-haggadah
+                                                        :on-click #(re-frame/dispatch [::push-state :haggadah-creation])}   "Create haggadah"]]
+      [:div
+       [:h1.is-size-3
+        "Haggadot created"]
+       (let [haggadot @(re-frame/subscribe [::subs/haggadot])]
+         (when haggadot
+           [:ul
+            (for [{:keys [title id]} haggadot :when id] 
+              ^{:key id}[:li.mb-2
+                         [:a {:data-testid (str "view-" id)
+                              :href (href :haggadah-view {:id id})} title]
+                         [:a.is-small.ml-2 {:data-testid (str "edit-" id )
+                                            :href (href :haggadah-edit {:id id})}
+                          [:i.fas.fa-edit]]])]))] ]]]
+   [wave-bottom]])
 
 (def edit-explanation
   "Source contains the haggadah with markdown, while preview shows you how the haggadah will appear after applying the markdown.
