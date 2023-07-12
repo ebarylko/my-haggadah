304d2c99cd8ae087a3af369b78aeca9947954358
EItan Barylko <eitan@barylko.com>
2023-07-12 08:13:20 -0700

    Added file for the dsl for making a haggadah

---
+++ src/haggadah/dsl.cljs
@@ -0,0 +1,20 @@
+(ns haggadah.dsl)
+
+
+(defn create-haggadah
+  "Pre: takes a title T and a bracha B
+  Post: returns a Haggadah with title T and bracha B"
+  [title bracha]
+  {:title title
+   :bracha bracha})
+
+(def bracha "סַבְרִי מָרָנָן וְרַבָּנָן וְרַבּוֹתַי. בָּרוּךְ אַתָּה ה', אֱלֹהֵינוּ מֶלֶךְ הָעוֹלָם בּוֹרֵא פְּרִי הַגָּפֶן")
+
+(defonce haggadah
+  (create-haggadah "Template" bracha))
+
+(defn parse-haggadah
+  "Pre: takes a Haggadah
+  Post: returns the same Haggadah represented in hiccup"
+  [{:keys [bracha]}]
+  [:div bracha])
