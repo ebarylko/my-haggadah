075c70e1c2105f1d6bd053cdff61b9136168fd8e
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2023-07-17 15:03:52 -0700

    create haggadah with song using dsl (#57)
    * Added a test for parsing a Haggadah which contains a song within

--- src/haggadah/dsl.cljs
+++ src/haggadah/dsl.cljs
@@ -15,14 +15,36 @@
 (defonce haggadah
   (create-haggadah "Wine" bracha))
 
+(defn create-haggadah-with-song
+  "Pre: takes a song and its title
+  Post: returns a Haggadah with song and title passed within"
+  [title song]
+  {:content 
+   {:song {:title title :content song}}})
+  
+
+(defn render-bracha
+  "Pre: takes a title and content for a bracha
+  Post: returns a hiccup representation of the bracha"
+  [title content]
+  [:div
+   [:div.has-text-centered.has-text-weight-bold.is-size-3.pb-2 {:data-testid :bracha-title} title]
+   [:div.has-text-right.is-size-5 {:data-testid :bracha-content} content]])
+
+(defn render-song
+  "Pre: takes a title and content for a song
+  Post: returns a hiccup representation of the song"
+  [title content]
+  [:div
+   [:div.has-text-centered.has-text-weight-bold.is-size-3.pb-2 title]
+   [:div.has-text-right.is-size-5 content]])
+
 (defn haggadah->hiccup
   [[k {:keys [title content]}]]
   (case k
-    :bracha [:div
-             [:div.has-text-centered.has-text-weight-bold.is-size-3.pb-2 {:data-testid :bracha-title} title]
-             [:div.has-text-right.is-size-5 {:data-testid :bracha-content }content]]
+    :bracha (render-bracha title content)
+    :song (render-song title content)
     :else [:div]))
-
 
 (defn parse-haggadah
   "Pre: takes a Haggadah
