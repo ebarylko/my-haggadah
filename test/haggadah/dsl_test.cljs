0b7e497cc8876feec7c846c0bc62d12c2b193440
EItan Barylko <eitan@barylko.com>
2023-07-19 21:37:36 -0700

    Doing some changes

--- test/haggadah/dsl_test.cljs
+++ test/haggadah/dsl_test.cljs
@@ -80,15 +80,62 @@
       (t/is (= haggadah-with-subsection hiccup-rep)))))
 
 
-(def haggadah-with-section
-  )
-
 (def ma-nishtana-content
   [["Ma nishtaná halaila azé micol aleilot?" "?מַה נִּשְׁתַּנָּה הַלַּיְלָה הַזֶּה מִכָּל הַלֵּילוֹת"]
    ["Shevejol haleilot anu ojlim jametz umatzá, halaila azé culó matzá" "שֶׁבְּכָל הַלֵּילוֹת אָנוּ אוֹכְלִין חָמֵץ וּמַצָּה, הַלַּיְלָה הַזֶּה - כּוּלוֹ מַצָּה"]
    ["Shevejol haleilot anu shear ierakot, halaila azé maror" "שֶׁבְּכָל הַלֵּילוֹת אָנוּ אוֹכְלִין שְׁאָר יְרָקוֹת - הַלַּיְלָה הַזֶּה מָרוֹר"]])
 
-#_(t/deftest haggadah-with-section-test
+(def ha-lachma-anya
+  [:div
+   [:div.has-text-centered.is-size-3.pb-2 ""]
+   [:div.pt-3
+    [:div.has-text-centered.is-size-5.pb-3 "Ha Lachma Anya"]
+    [:div.has-text-right.is-size-5 "הָא לַחְמָא עַנְיָא דִּי אֲכָלוּ אַבְהָתָנָא בְאַרְעָא דְמִצְרָיִם. כָּל דִכְפִין יֵיתֵי וְיֵיכֹל, כָּל דִצְרִיךְ יֵיתֵי וְיִפְסַח. הָשַּׁתָּא הָכָא, לְשָׁנָה הַבָּאָה בְּאַרְעָא דְיִשְׂרָאֵל. הָשַּׁתָּא עַבְדֵי, לְשָׁנָה הַבָּאָה בְּנֵי חוֹרִין"]]])
+
+
+[["Ma nishtaná halaila azé micol aleilot?" "?מַה נִּשְׁתַּנָּה הַלַּיְלָה הַזֶּה מִכָּל הַלֵּילוֹת"]
+ ["Shevejol haleilot anu ojlim jametz umatzá, halaila azé culó matzá" "שֶׁבְּכָל הַלֵּילוֹת אָנוּ אוֹכְלִין חָמֵץ וּמַצָּה, הַלַּיְלָה הַזֶּה - כּוּלוֹ מַצָּה"]
+ ["Shevejol haleilot anu shear ierakot, halaila azé maror" "שֶׁבְּכָל הַלֵּילוֹת אָנוּ אוֹכְלִין שְׁאָר יְרָקוֹת - הַלַּיְלָה הַזֶּה מָרוֹר"]]
+
+(def ma-nishtana
+  [:div
+   [:div.has-text-centered.is-size-3.pb-2 ""]
+   [:div.pt-3
+    [:div.has-text-centered.pb-4.is-size-5 "The Four Questions"]
+    [:table.is-bordered.is-flex.is-justify-content-center.table
+     [:tbody
+      [:tr
+       [:td "Sangre"]
+       [:td "דָּם"]]
+      [:tr  
+       [:td "Ranas"]
+       [:td "צְפַרְדֵּעַ"]]
+      [:tr  
+       [:td "Piojos"]
+       [:td "כִּנִּים"]]
+      [:tr
+       [:td "Bestias"]
+       [:td "עָרוֹב"]]
+      [:tr  
+       [:td "Peste"]
+       [:td "דֶּבֶר"]]]]]
+   ]
+  )
+
+(def haggadah-with-section
+  [:div
+   [:div.has-text-centered.has-text-weight-bold.is-size-4 "Magid"]
+   [:div
+    [:div.has-text-centered.is-size-3.pb-2 ""]
+    [:div.pt-3
+     [:div.has-text-centered.is-size-5.pb-3 "Ha Lachma Anya"]
+     [:div.has-text-right.is-size-5 "הָא לַחְמָא עַנְיָא דִּי אֲכָלוּ אַבְהָתָנָא בְאַרְעָא דְמִצְרָיִם. כָּל דִכְפִין יֵיתֵי וְיֵיכֹל, כָּל דִצְרִיךְ יֵיתֵי וְיִפְסַח. הָשַּׁתָּא הָכָא, לְשָׁנָה הַבָּאָה בְּאַרְעָא דְיִשְׂרָאֵל. הָשַּׁתָּא עַבְדֵי, לְשָׁנָה הַבָּאָה בְּנֵי חוֹרִין"]]]
+   ma-nishtana
+
+   ])
+
+
+(t/deftest haggadah-with-section-test
   (t/testing "When the user creates a Haggadah with a section and the Haggadah is parsed, the correct hiccup representation of the Haggadah is returned"
     (let [section-title "Magid"
           first-song-title "Ha Lachma Anya" 
@@ -97,7 +144,7 @@
           table-content ma-nishtana-content
           second-song-title "We Were Slaves in Egypt"
           second-song-content "עֲבָדִים הָיִינוּ לְפַרְעֹה בְּמִצְרָיִם, וַיּוֹצִיאֵנוּ ה' אֱלֹהֵינוּ מִשָּׁם בְּיָד חֲזָקָה וּבִזְרֹעַ נְטוּיָה."
-          section-content [{:subsection {:title "" :content {:song {:title song-title :content song-content}}}}
+          section-content [{:subsection {:title "" :content {:song {:title first-song-title :content first-song-content}}}}
                            {:subsection {:title "" :content {:table {:title table-title :content table-content}}}}
                            {:subsection {:title "" :content {:song {:title second-song-title :content second-song-content}}}}]
           haggadah (dsl/create-haggadah-with-section section-title section-content)
