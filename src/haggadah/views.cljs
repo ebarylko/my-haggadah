50eb0bd369cce2fd17499112208d4c8d37be85c8
EItan Barylko <eitan@barylko.com>
2023-07-22 05:36:44 -0700

    Removed unused code

--- src/haggadah/views.cljs
+++ src/haggadah/views.cljs
@@ -200,45 +200,6 @@
                                                                                          %])
                               :id "submit"} "Create"]]]]]]]))
 
-(def table
-   [:div
-    [:div.has-text-centered.is-size-3.pb-2 ""]
-    [:div.pt-3
-     [:div.has-text-centered.pb-4.is-size-5 "The four questions"]
-     [:table.is-bordered.is-flex.is-justify-content-center.table
-      [:tbody
-       [:tr
-        [:td "Ma nishtaná halaila azé micol aleilot?"]
-        [:td "מַה נִּשְׁתַּנָּה הַלַּיְלָה הַזֶּה מִכָּל הַלֵּילוֹת"]]]]]])
-
-(def song
-  [:div
-   [:div.has-text-centered.is-size-3.pb-2 ""]
-   [:div.pt-3
-    [:div.has-text-centered.is-size-5.pb-3 "Ha Lachma Anya"]
-    [:div.has-text-right.is-size-5 "הָא לַחְמָא עַנְיָא דִּי אֲכָלוּ אַבְהָתָנָא בְאַרְעָא דְמִצְרָיִם. כָּל דִכְפִין יֵיתֵי וְיֵיכֹל, כָּל דִצְרִיךְ יֵיתֵי וְיִפְסַח. הָשַּׁתָּא הָכָא, לְשָׁנָה הַבָּאָה בְּאַרְעָא דְיִשְׂרָאֵל. הָשַּׁתָּא עַבְדֵי, לְשָׁנָה הַבָּאָה בְּנֵי חוֹרִין"]]])
-
-(def song-2
-  [:div
-   [:div.has-text-centered.is-size-3.pb-2 ""]
-   [:div.pt-3
-    [:div.has-text-centered.is-size-5.pb-3 "We Were slaves in Egypt"]
-    [:div.has-text-right.is-size-5 "עֲבָדִים הָיִינוּ לְפַרְעֹה בְּמִצְרָיִם, וַיּוֹצִיאֵנוּ ה' אֱלֹהֵינוּ מִשָּׁם בְּיָד חֲזָקָה וּבִזְרֹעַ נְטוּיָה."]]])
-
-(def section
- [:div
-  [:div.has-text-centered.has-text-weight-bold.is-size-4 "Magid"]
-  song
-  table
-  song-2])
-
-(def table2
-  (dsl/table "Las Diez Plagas" 
-             (dsl/row ["Sangre" "דָּם"])
-             (dsl/row ["Ranas" "צְפַרְדֵּעַ"])))
-
-
-
 (defn haggadah-view-panel
   []
   [:div.page.is-flex.is-flex-grow-1 {:class (styles/haggadah-view)}
