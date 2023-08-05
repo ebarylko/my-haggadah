e03a4dba1820b29bf477fd22f3287343318b291b
EItan Barylko <eitan@barylko.com>
2023-08-05 16:41:55 -0700

    Added new test for checking that the cancel button works

---
+++ test/acceptance/page_navigation_test.clj
@@ -0,0 +1,50 @@
+(ns acceptance.page-navigation-test
+  (:require  [clojure.test :as t]
+             [etaoin.api :as e]
+             [acceptance.core :as c :refer [driver]]
+             ))
+
+(defn ->home
+  "Pre: takes nothing
+  Post: takes you to the home page"
+  []
+  (e/go driver "http://localhost:5000/"))
+
+(defn home->login
+  "Pre: takes nothing
+  Post: takes you from the home page to the login page"
+  []
+  (doto driver
+    (e/go "http://localhost:5000/")
+   (e/click-visible {:data-testid :login})
+   (e/wait-visible {:data-testid :submit})))
+
+(defn click-on-cancel-button
+  "Pre: takes nothing
+  Post: clicks on the cancel button on the page"
+  []
+  (e/click-visible {:data-testid :cancel}))
+
+(defn wait-for-home-page
+  "Pre: takes nothing
+  Post: waits for the home page to the rendered"
+  []
+  (e/wait-visible {:data-testid :login}))
+
+(defn at-home-page?
+  []
+  (e/exists? driver {:data-testid :login}))
+
+(defn current-page
+  "Pre: takes nothing
+  Post: returns the current page the user is on"
+  []
+  (cond
+    (at-home-page?) :home-page))
+
+(t/deftest click-cancel-button-test
+  (t/testing "When the user goes to the login page and clicks the cancel button, they should return to the home page"
+    (home->login)
+    (click-on-cancel-button)
+    (wait-for-home-page)
+    ))
