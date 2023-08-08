97c75649aa02e2cbd79a54120eb8dafc2af7e5a4
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2023-08-08 10:35:20 -0700

    Add english and hebrew content for each song (#90)
    * Modified song so it takes English and Hebrew content

--- test/haggadah/dsl_test.cljs
+++ test/haggadah/dsl_test.cljs
@@ -16,10 +16,13 @@
 
 (t/deftest render-song-test
   (t/testing "When rendering the song, returns the title and content"
-    (let [song (dsl/song "Ki lo nae" "כִּי לוֹ נָאֶה, כִּי לוֹ יָאֶה. אַדִּיר בִּמְלוּכָה, בָּחוּר כַּהֲלָכָה, גְּדוּדָיו")
+    (let [song (dsl/song "Ki lo nae"
+                         "כִּי לוֹ נָאֶה, כִּי לוֹ יָאֶה. אַדִּיר בִּמְלוּכָה, בָּחוּר כַּהֲלָכָה, גְּדוּדָיו"
+                         "Since for Him it is pleasant, for Him it is suited." )
           expected [:div.song
                     [:div.title "Ki lo nae" ]
-                    [:div.text "כִּי לוֹ נָאֶה, כִּי לוֹ יָאֶה. אַדִּיר בִּמְלוּכָה, בָּחוּר כַּהֲלָכָה, גְּדוּדָיו"]]]
+                    [:div.text.hebrew.pb-3 "כִּי לוֹ נָאֶה, כִּי לוֹ יָאֶה. אַדִּיר בִּמְלוּכָה, בָּחוּר כַּהֲלָכָה, גְּדוּדָיו"]
+                    [:div.english-text "Since for Him it is pleasant, for Him it is suited."]]]
       (t/is (= expected (dsl/render-haggadah song))))))
 
 (t/deftest render-table-test
