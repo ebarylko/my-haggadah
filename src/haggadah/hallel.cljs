122db14e9198f4f7e5fbdda0e1b0fd2bbf192318
EItan Barylko <eitan@barylko.com>
2023-08-13 19:33:00 -0700

    Finished praise-and-thanks

--- src/haggadah/hallel.cljs
+++ src/haggadah/hallel.cljs
@@ -1,10 +1,13 @@
 (ns haggadah.hallel
   (:require [haggadah.dsl :as dsl :refer [instruction section general-content bracha-with-more-content general-content-with-instruction]]
-            [haggadah.hallel-second :refer [second-half]]))
+            [haggadah.hallel-second :refer [second-half]]
+            [haggadah.hallel-praise :refer [praise-and-thanks]]
+            ))
 
 
 (def hallel
   (section "הַלֵּל"
            "Hallel"
            second-half
+           praise-and-thanks
            ))
