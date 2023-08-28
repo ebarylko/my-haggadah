62cf454b3e7dabec60e3ed8b136e0624cdb3071e
EItan Barylko <eitan@barylko.com>
2023-08-27 23:10:15 -0700

    Added fs babashka library to file for setting firebase emulator vars

--- set_vars.clj
+++ set_vars.clj
@@ -1,55 +1,50 @@
 (ns haggadah.set-vars
   (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]
-            #_[babashka.fs :refer [exists?]]))
+            [babashka.fs :refer [exists?]]))
 
 (def base "127.0.0.1:")
 (def emulator-hub "export FIREBASE_EMULATOR_HUB=127.0.0.1:4400\n")
 (def fire-config "export FIREBASE_CONFIG={\"projectId\" \"my-haggadah\" \"storageBucket\" \"my-haggadah.appspot.com\" \"databaseURL\" \"https://my-haggadah.firebaseio.com\"}\n")
 
-(defn all-ports
+(def not-valid (every-pred nil? (complement string?)))
+
+(defn parse-ports
   "Pre: takes a file which includes the emulator ports
   Post: returns all the emulator ports"
   [file]
-  #_(try
-    (-> file
-        ((partial slurp))
-        (json/read-str :key-fn keyword)
-        (:emulators))
-    (catch Exception e (str "The config file passed does not exist "))
-    )
-  (cond
-    (or (nil? file ) (not (.exists (io/file file)) )) (throw (Exception. "The config file does not exist"))
-    #_(exists? file) 
-    :else (-> file
-              ((partial slurp))
-              (json/read-str :key-fn keyword)
-              (:emulators))))
+  (-> file
+      slurp
+      (json/read-str :key-fn keyword)
+      :emulators))
 
 (defn get-port
   "Pre: takes a collection of emulators and an emulator name
   Post: returns the port of that emulator"
   [emulator name]
-(get-in emulator [name :port] "package.json"))
+(get-in emulator [name :port]))
 
-(def num->emu-config
- {1 "firebase.json"
-  2 "test/acceptance_emulators/firebase.json"
-  3 "functions/cloud_emulators/firebase.json"
-  4 "test/unit_emulators/firebase.json"
-  5 "local_emulators"})
+(def env->emu-config
+ {:prod "firebase.json"
+  :acceptance "test/acceptance_emulators/firebase.json"
+  :functions "functions/cloud_emulators/firebase.json"
+  :unit "test/unit_emulators/firebase.json"
+  :local "local_emulators"})
+
+(def mk-key (comp keyword first))
+
+(def emu-config (comp env->emu-config mk-key))
 
 (defn set-vars
-  "Pre: takes a file which contains a collection of key value pairs of the form [emulator-name emulator-port]
-  Post: exports the ports of the auth and firestore emulators"
-  [file]
-  (println "the file " file)
-  (let [ports (all-ports file)
-        #_#_[fs-port auth-port] (map (partial get-port ports) [:firestore :auth])
-        #_#_fs-var (format "export FIREBASE_FIRESTORE_EMULATOR_ADDRESS=%s%d\n" base fs-port)
-        #_#_auth-var (format "export FIREBASE_AUTH_EMULATOR_HOST=%s%d\n" base auth-port)]
+  "Pre: takes a string which represents an environment (prod, local, etc)
+  Post: Sets the authentication and firestore emulators ports for that environment"
+  [[file]]
+  {:pre [(exists? file)]}
+  (let [ports (parse-ports file)
+        [fs-port auth-port] (map (partial get-port ports) [:firestore :auth])
+        fs-var (format "export FIREBASE_FIRESTORE_EMULATOR_ADDRESS=%s%d\n" base fs-port)
+        auth-var (format "export FIREBASE_AUTH_EMULATOR_HOST=%s%d\n" base auth-port)]
     (spit ".env.test" "")
-    (doseq [var [emulator-hub fire-config #_#_fs-var auth-var]]
+    (doseq [var [emulator-hub fire-config fs-var auth-var]]
       (spit ".env.test" var :append true))))
 
 (set-vars *command-line-args*)
